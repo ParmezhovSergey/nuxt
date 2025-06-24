c6df8d4e53e77d60cb2ac71df548dcf453759c32
ParmezhovSergey <parmezhovsergey@mail.ru>
2025-06-24 20:19:12 +0300

    1

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <header>
     <!-- <button @click="router.push(`/about`)"> About</button> -->
-    <NuxtLink to="/nuxt/about">About</NuxtLink>
-    <NuxtLink to="/nuxt/contact">Contacts</NuxtLink>
+    <NuxtLink to="/about">About</NuxtLink>
+    <NuxtLink to="/contact">Contacts</NuxtLink>
   </header>
   <main>
     <slot></slot>
